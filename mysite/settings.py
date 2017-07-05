924bd2afbea9f8db66a12c36f16a4431057e228e
admin <admin@admin.com>
2017-07-05 11:34:56 +0530

    Added option to delete post and logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statc')
+LOGIN_REDIRECT_URL = '/'
